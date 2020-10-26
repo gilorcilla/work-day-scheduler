7f63da1689061c1374995f69f1dc5d8f9e86665c
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-26 16:43:41 -0600

    Completed Local Storage functionality

---
+++ assets/js/jsscript.js
@@ -0,0 +1,12 @@
+var today = moment().format();
+$("#currentDay").text(today);
+$(".saveBtn").on("click", function () {
+  var time = $(this).attr("id").split("-")[1];
+  var userData = $("#e-" + time).val();
+  console.log(time, userData);
+  localStorage.setItem(time, userData);
+});
+var currentTime = moment().hours();
+for (let i = 9; i <= 17; i++) {
+  $("#e-" + i).val(localStorage.getItem(i));
+}
